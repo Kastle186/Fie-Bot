7bd82bf18d474e5a2b3d950ed69c6cb1d1990260
Yoxide <ducostamiguel@gmail.com>
2025-07-01 19:55:37 +0000

    Trails Talking XP 1.2.2 - Placed the constants in a different file to make the system easier to read

---
+++ fieconstants.py
@@ -0,0 +1,48 @@
+CHARACTER_LEVEL_IMAGES = {
+    "fie": {
+        1: "images/Fie CS I.png",
+        2: "images/Fie CS II.png",
+        3: "images/Fie CS III.png",
+        4: "images/Fie Reverie.png",
+        5: "images/Fie Daybreak.png",
+    },
+    "renne": {
+        1: "images/Renne SC.png",
+        2: "images/Renne 3rd.png",
+        3: "images/Renne Zero.png",
+        4: "images/Renne Azure.png",
+        5: "images/Renne CS IV.png",
+        6: "images/Renne Daybreak.png",
+        7: "images/Renne Daybreak II.png",
+    },
+    "laura": {
+        1: "images/Laura CS I.png",
+        2: "images/Laura CS II.png",
+        3: "images/Laura CS III.png",
+        4: "images/Laura Reverie.png",
+    },
+    "rean": {
+        1: "images/Rean CS I.png",
+        2: "images/Rean CS II.png",
+        3: "images/Rean CS III.png",
+        4: "images/Rean CS IV.png",
+        5: "images/Rean Reverie.png",
+        6: "images/Rean Kai.png",
+    },
+    "emma": {
+        1: "images/Emma CS I.png",
+        2: "images/Emma CS II.png",
+        3: "images/Emma CS III.png",
+        4: "images/Emma CS IV.png",
+        5: "images/Emma Reverie.png",
+    }
+}
+
+LEVEL_THRESHOLDS = [0, 1000, 2500, 5000, 10000, 15000, 20000]
+
+rank_points = {
+    "S": 3,
+    "A": 3,
+    "B": 2,
+    "C": 1
+}
